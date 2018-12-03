6bc74605d6ed2cc014c7c26550b7c0a3a5ae539a
Stefan Roche <stefanroche@hotmail.com>
2018-12-03 12:44:50 +0000

    Update RegisterSystem.py

--- Client/Bin/RegisterSystem.py
+++ Client/Bin/RegisterSystem.py
@@ -1,14 +1,14 @@
 import socket
 import getpass
 import hashlib
-fh = open("login.txt", "w")
+fh = open("data/login.txt", "w")
 fh.writelines("bad")
 fh.close()
 passwordsame = "f"
 inc = False
 s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 4243
-fh = open("ip.txt", "r")
+fh = open("data/ip.txt", "r+")
 ip2 = fh.read()
 ipchange = input("Do you want to manually set IP of server? (Y/N): ")
 if ipchange == "Y" or "y":
@@ -16,9 +16,13 @@ if ipchange == "Y" or "y":
 else:
     ip2 = ip2
 fh.close()
-s2.connect((ip2, port))
+try:
+    s2.connect((ip2, port))
+except:
+    print('REGISTRATION SERVER IS OFFLINE RIGHT NOW')
+    exit()
 user = input("Username: ")
-fh = open("user.txt", "w")
+fh = open("data/user.txt", "w")
 fh.writelines(user)
 fh.close()
 global pswd
@@ -57,6 +61,6 @@ if returned == "register":
     print("Registration Success")
 else:
     print("Registration Failed")
-fh = open("login.txt", "w")
+fh = open("data/login.txt", "w")
 fh.writelines(returned)
 fh.close()
