a478e086ac113847156605e98a7288a3459c068a
Stefan Roche <stefanroche@hotmail.com>
2018-12-07 13:36:47 +0000

    Update clientpass.py

--- Client/Bin/clientpass.py
+++ Client/Bin/clientpass.py
@@ -6,16 +6,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 4242
 fh = open("data/user.txt","w")
 fh.close()
-fh = open("data/user.txt","r+")
+fh = open("data/user.txt","r")
 uname = fh.read()
 fh.close()
-fh = open("data/ip.txt", "r+")
+fh = open("data/ip.txt", "r")
 ip = fh.read()
-ipchange = input("Do you want to manually set IP of server? (Y/N): ")
-if ipchange == "Y" or "y":
-    ip = input("What do you want to set the ip to?: ")
-else:
-    ip = ip
 fh.close()
 s.connect((ip, port))
 s.send(uname.encode('ascii'))
